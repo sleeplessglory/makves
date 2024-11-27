c2304ecc4cf34cb0432365d33b0f51c7f00ec1c4
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2024-11-28 01:08:39 +0300

    chore: set the tsconfig, react project and boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import Sidebar from './components/Sidebar';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <StrictMode>
+    <Sidebar />
+  </StrictMode>,
+);
