ab8b932ed887694e563634722bc8f544b2989b39
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2024-11-28 16:36:59 +0300

    feat: added mapping through the buttons

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+const buttons = [
+    { id: 1, "name": "Dashboard"}, 
+    { id: 2, "name": "Sales"}, 
+    { id: 3, "name": "Costs"}, 
+    { id: 4, "name": "Payments"}, 
+    { id: 5, "name": "Finances"}, 
+    { id: 6, "name": "Messages"}, 
+    { id: 7, "name": "Settings"}, 
+    { id: 8, "name": "Support"}];
+
+export default buttons;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,17 @@
-export default function Sidebar() {
+import buttons from './Button';
+
+type SidebarProps = {
+    colour: string,
+}
+
+export default function Sidebar({colour}: SidebarProps) {
     return (
-        <>
-        
-        </>
+        <nav className="sidebar">
+            {buttons.map((button) =>
+                <button key={button.id}>
+                    <span>{button.name}</span>
+                </button>
+            )}
+        </nav>
     );
 };
